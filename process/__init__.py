5304e1648db0d674183268a819ed4975efb78a61
Alex Hagen <ahagen@purdue.edu>
2017-11-28 15:13:09 -0500

    converting to python 2 and turning it into a python app

--- process/__init__.py
+++ process/__init__.py
@@ -8,6 +8,7 @@ import os.path as P
 from collections import deque
 from fnmatch import fnmatch
 import re
+import io
 
 PROC_DIR = '/proc'
 time_now = time.time
@@ -65,7 +66,7 @@ class ProcessByPID:
         self.status_path = P.join(path, 'status')
 
         # Get the command that started the process
-        with open(P.join(path, 'cmdline'), encoding='utf-8') as f:
+        with io.open(P.join(path, 'cmdline'), encoding='utf-8') as f:
             cmd = f.read()
             # args are separated by \x00 (Null byte)
             self.command = cmd.replace('\x00', ' ').strip()
@@ -195,6 +196,13 @@ class ProcessIDs:
         return self
 
     def __next__(self):
+        if self._new_pids:
+            return self._new_pids.popleft()
+
+        else:
+            raise StopIteration
+
+    def next(self):
         if self._new_pids:
             return self._new_pids.popleft()
 
